2b0959049c8e370236b78652b5e07cb8bb6915bb
Maniacf <maniac@ua.fm>
2013-03-06 15:43:24 +0200

    add Master Pages

---
+++ MasterPages/MainMasterPage.Master.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Web.Security;
+using System.Web.UI.HtmlControls;
+using BizObj.Data;
+using BizObj.Document;
+using PermissionMembership;
+
+namespace Document.MasterPages
+{
+    public partial class MainMasterPage : UI.DocumentMasterPage
+    {
+        protected void InsertAppSettings()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("var appSettings = {");
+            stringBuilder.Append(String.Format("rootUrl: \"{0}\"", RootFullURL));
+            stringBuilder.Append("}");
+
+            HtmlGenericControl scriptControl = new HtmlGenericControl("script");
+            scriptControl.Attributes["type"] = "text/javascript";
+            scriptControl.InnerHtml = stringBuilder.ToString();
+            Page.Header.Controls.AddAt(0, scriptControl);
+        }
+
+        protected void InsertNavigationSettings()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("var navigationSettings = {");
+            string menuList = String.Empty;
+            string menuTemplateList = String.Empty;
+
+            MembershipUser mu = Membership.GetUser();
+            if (mu != null && mu.ProviderUserKey != null)
+            {
+                SqlConnection connection = new SqlConnection(Config.ConnectionString);
+                try
+                {
+                    connection.Open();
+                    SqlTransaction trans = null;
+                    try
+                    {
+                        trans = connection.BeginTransaction();
+                                                
+                        DataTable dt = AccessUser.RoleAndObjectsByUserList(trans, (Guid)mu.ProviderUserKey, Department.ObjectTypeID).Table;
+                        if (dt != null)
+                        {
+                            foreach (DataRow row in dt.Rows)
+                            {
+                                Guid objectID = (Guid)row["ObjectID"];
+                                Department department = new Department(objectID, mu.UserName);
+                                menuList += "{";
+                                menuList += String.Format(" id:{0}, name: \"{1}\" ", department.ID, department.ShortName);
+                                menuList += "},";
+                            }
+                        }
+
+                        DataTable mt = AccessUser.RoleAndObjectsByUserList(trans, (Guid)mu.ProviderUserKey, MenuTemplate.ObjectTypeID).Table;
+                        if (mt != null)
+                        {
+                            foreach (DataRow row in mt.Rows)
+                            {
+                                Guid objectID = (Guid)row["ObjectID"];
+                                MenuTemplate menuTemp = new MenuTemplate(objectID, mu.UserName);
+                                menuTemplateList += "{";
+                                menuTemplateList += String.Format(" id:{0}, name: \"{1}\", systemName: \"{2}\"  ", menuTemp.ID, menuTemp.Name, menuTemp.SystemName);
+                                menuTemplateList += "},";
+                            }
+                        }
+                        /*
+                        List<Department> dl = Department.GetList(mu.UserName);
+                        foreach (Department department in dl)
+                        {
+                            if (Permission.IsUserPermission(trans, (Guid) mu.ProviderUserKey, department.ObjectID, (int)Department.ActionType.View))
+                            {
+                                menuList += "{";
+                                menuList += String.Format(" id:{0}, name: \"{1}\" ", department.ID, department.ShortName);
+                                menuList += "},";
+                            }
+                        }
+                        */
+                        
+                        trans.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        if (trans != null)
+                            trans.Rollback();
+                        throw;
+                    }
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            char[] charsToTrim = { ',' };
+            stringBuilder.Append(String.Format("menuList: [{0}],", menuList.TrimEnd(charsToTrim)));
+            stringBuilder.Append(String.Format("tamplateList: [{0}]", menuTemplateList.TrimEnd(charsToTrim)));
+            stringBuilder.Append("}");
+
+            HtmlGenericControl scriptControl = new HtmlGenericControl("script");
+            scriptControl.Attributes["type"] = "text/javascript";
+            scriptControl.InnerHtml = stringBuilder.ToString();
+            Page.Header.Controls.AddAt(1, scriptControl);
+        }
+
+        protected string FormatDictionaryList(DataTable dt, string idColumnName)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow r = dt.Rows[i];
+                sb.Append("{ id:" + r[idColumnName] + ", ");
+                sb.Append("name: \"" + r["Name"] + "\" }");
+                if (i != dt.Rows.Count - 1)
+                    sb.Append(", ");
+            }
+
+            return sb.ToString();
+        }
+
+        protected void InsertAppData()
+        {
+            StringBuilder appDataSb = new StringBuilder();
+            appDataSb.Append("var appData = {");
+            appDataSb.Append("dictionaries: {");
+            appDataSb.Append("inputDocTypes: [" + FormatDictionaryList(InputDocType.GetAll(), "InputDocTypeID") + "], ");
+            appDataSb.Append("inputMethods: [" + FormatDictionaryList(InputMethod.GetAll(), "InputMethodID") + "], ");
+            appDataSb.Append("inputSigns: [" + FormatDictionaryList(InputSign.GetAll(), "InputSignID") + "], ");
+            appDataSb.Append("inputSubjectTypes: [" + FormatDictionaryList(InputSubjectType.GetAll(), "InputSubjectTypeID") + "], ");
+            appDataSb.Append("deliveryTypes: [" + FormatDictionaryList(DeliveryType.GetAll(), "DeliveryTypeID") + "] ,");
+            appDataSb.Append("socialStatuses: [" + FormatDictionaryList(SocialStatus.GetAll(), "SocialStatusID") + "], ");
+            appDataSb.Append("branchTypes: [" + FormatDictionaryList(BranchType.GetAll(), "BranchTypeID") + "], ");
+            appDataSb.Append("socialCategories: [" + FormatDictionaryList(SocialCategory.GetAll(), "SocialCategoryID") + "], ");
+            appDataSb.Append("cardStatuses: [" + FormatDictionaryList(CardStatus.GetAll(), "CardStatusID") + "]");
+            appDataSb.Append("}");
+            appDataSb.Append("}");
+
+            HtmlGenericControl scriptControl = new HtmlGenericControl("script");
+            scriptControl.Attributes["type"] = "text/javascript";
+            scriptControl.InnerHtml = appDataSb.ToString();
+            Page.Header.Controls.AddAt(0, scriptControl);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsLogged())
+            {
+                LoginStatusMain.Visible = false;
+            }
+
+            InsertAppData();
+            InsertAppSettings();
+            InsertNavigationSettings();
+        }
+
+        public override void SetErrorMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetErrorMessage(List<string> messages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetMessage(List<string> messages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetWarningMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetWarningMessage(List<string> messages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MasterPages/MainMasterPage.Master.designer.cs
@@ -0,0 +1,78 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace Document.MasterPages {
+    
+    
+    public partial class MainMasterPage {
+        
+        /// <summary>
+        /// appSettingsLiteral элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal appSettingsLiteral;
+        
+        /// <summary>
+        /// cphHead элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cphHead;
+        
+        /// <summary>
+        /// form1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// LoginNameMain элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginName LoginNameMain;
+        
+        /// <summary>
+        /// LoginStatusMain элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginStatus LoginStatusMain;
+        
+        /// <summary>
+        /// ucMenu элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::Document.Controls.Master.Menu ucMenu;
+        
+        /// <summary>
+        /// cphBodyMain элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder cphBodyMain;
+    }
+}
